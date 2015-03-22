c03aa81c56b54e5cfbb4069925ec232819a68e46
Vincenzo Minolfi <vincenzo.minolfi@gmail.com>
2015-03-22 11:12:36 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # angular-datetimepicker
-A simple AngularJS directive for a datetimepicker
+
+A simple AngularJS directive for a datetimepicker that takes up little space on the screen, and comes with two different styles.
+
+Style contains two styles, which can be used in the setting file template classes "style1" and "style2".
+
+![style1](/screenshots/style1.png?raw=true)
+![style2](/screenshots/style2.png?raw=true)
---
+++ app.js
@@ -0,0 +1,122 @@
+moment.locale(window.navigator.userLanguage || window.navigator.language || 'it');
+
+var app = angular.module('myApp', []);
+
+
+app.controller('AppController', ['$scope', function ($scope) {
+  $scope.date = null;//new Date(2011, 10, 2, 11, 14, 54, 876);
+}]);
+
+
+app.directive('vmDatetimepicker', function() {
+  return {
+    restrict: 'AE',
+    require: '^ngModel',
+    scope: {
+      ngModel: '='
+    },
+    templateUrl: 'vmDatePicker-template.html',
+    link: function(scope, iElement, iAttrs) {
+      // get details
+    },
+    controller: ['$scope', function($scope) {
+      $scope.moment = moment($scope.ngModel || new Date());
+      $scope.d = 0;
+      $scope.M = 0;
+      $scope.y = 0;
+      $scope.h = 0;
+      $scope.m = 0;
+      
+      $scope.refreshModels = function() {
+        $scope.d = $scope.moment.date();
+        $scope.M = $scope.moment.month()+1;
+        $scope.y = $scope.moment.year();
+        $scope.h = $scope.moment.hour();
+        $scope.m = $scope.moment.minute();
+        $scope.weekday = $scope.moment.format('dddd');
+        $scope.ngModel = $scope.moment.toDate();
+      };
+      $scope.refreshModels();
+      
+      $scope.$watch('d', function(newValue, oldValue) {
+        if(oldValue!=newValue) {
+            $scope.moment.date(newValue);
+            $scope.refreshModels();
+        }
+   		}, false);
+      
+      $scope.$watch('M', function(newValue, oldValue) {
+        //alert(newValue+"."+oldValue)
+        if(oldValue!=newValue) {
+	        $scope.moment.month(newValue-1);
+        	$scope.refreshModels();
+        }
+   		}, false);
+      
+      $scope.$watch('y', function(newValue, oldValue) {
+        //alert(newValue+"."+oldValue)
+        if(oldValue!=newValue) {
+	        $scope.moment.year(newValue);
+        	$scope.refreshModels();
+        }
+   		}, false);
+      
+      $scope.$watch('h', function(newValue, oldValue) {
+        //alert(newValue+"."+oldValue)
+        if(oldValue!=newValue) {
+	        $scope.moment.hour(newValue);
+        	$scope.refreshModels();
+        }
+   		}, false);
+      
+      $scope.$watch('m', function(newValue, oldValue) {
+        //alert(newValue+"."+oldValue)
+        if(oldValue!=newValue) {
+	        $scope.moment.minute(newValue);
+        	$scope.refreshModels();
+        }
+   		}, false);
+      
+      $scope.addDay = function(){
+        $scope.moment.add(1, "days");
+        $scope.refreshModels();
+      };
+      $scope.removeDay = function(){
+        $scope.moment.subtract(1, "days");
+        $scope.refreshModels();
+      };
+      $scope.addMonth = function(){
+        $scope.moment.add(1, "months");
+        $scope.refreshModels();
+      };
+      $scope.removeMonth = function(){
+        $scope.moment.subtract(1, "months");
+        $scope.refreshModels();
+      };
+      $scope.addYear = function(){
+        $scope.moment.add(1, "years");
+        $scope.refreshModels();
+      };
+      $scope.removeYear = function(){
+        $scope.moment.subtract(1, "years");
+        $scope.refreshModels();
+      };
+      $scope.addHour = function(){
+        $scope.moment.add(1, "hours");
+        $scope.refreshModels();
+      };
+      $scope.removeHour = function(){
+        $scope.moment.subtract(1, "hours");
+        $scope.refreshModels();
+      };
+      $scope.addMinute = function(){
+        $scope.moment.add(1, "minutes");
+        $scope.refreshModels();
+      };
+      $scope.removeMinute = function(){
+        $scope.moment.subtract(1, "minutes");
+        $scope.refreshModels();
+      };
+    }]
+  }
+});
